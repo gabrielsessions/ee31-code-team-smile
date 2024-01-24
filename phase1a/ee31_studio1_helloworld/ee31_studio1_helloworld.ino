4ddb167919525bee7060f84a12fde3687ee7d0a5
Philip Lengyel <philip.lengyel@tufts.edu>
2024-01-24 18:05:31 -0500

    finish potentiometer

--- phase1a/ee31_studio1_helloworld/ee31_studio1_helloworld.ino
+++ phase1a/ee31_studio1_helloworld/ee31_studio1_helloworld.ino
@@ -1,9 +1,8 @@
 #define BLUE_LED 6
-#define GREEN_LED 3
+#define GREEN_LED 9
 #define RED_LED 5
-#define SWITCH1 12
-#define SWITCH2 13
-
+#define SWITCH1 2
+#define SWITCH2 3
 #define FADE_AMOUNT 5
 #define TIME_CONSTANT 6000
 
@@ -17,7 +16,8 @@ enum State {
 
 State ledState;
 int greenLEDBrightness, blueLEDBrightness;
-
+int blueLEDInterval = 1;
+int global_brightness = 100;
 
 void setup() {
   // put your setup code here, to run once:
@@ -29,38 +29,65 @@ void setup() {
   pinMode(RED_LED, OUTPUT); // RED
   pinMode(SWITCH1, INPUT); // Switch 1
   pinMode(SWITCH2, INPUT); // Switch 2
+  attachInterrupt(digitalPinToInterrupt(SWITCH1), trigSw1, RISING);
+  attachInterrupt(digitalPinToInterrupt(SWITCH2), trigSw2, FALLING);
   Serial.begin(9600);  // initialize serial communication at 9600 bits per second
+  
+}
+/* 
+In the Run state produce the desired functionality:  
+1. when switch #1 is moved from OPEN to CLOSED, an interrupt is triggered 
+which changes the rate at which a BLUE LED flashes to 
+ten times per second from a rate of once per second.
 
+2. When switch #2 is moved from CLOSED to OPEN, an interrupt is triggered 
+only if the flash rate of the BLUE LED is ten times per second, by turning on a RED LED.  
+
+When BLUE LED flash rate is once per second the RED LED is off.
+
+ */
+void trigSw1(){
+  if(ledState = RUN){
+    blueLEDInterval = 10;
+  }
+}
+void trigSw2(){
+  if(ledState == RUN and blueLEDInterval == 10){
+    digitalWrite(RED_LED, HIGH);
+  }
 }
 
-// the loop routine runs over and over again forever:
-void usePotentiometer() {
-  //A5 = Potentiometer 1
-  //A4 = Potentiometer 2
-
-
-  // Start reading potentiometer values
-  int potentiometer1 = analogRead(A5);
-  int potentiometer2 = analogRead(A4);
-
-  //Lets just print it for now
-  Serial.println(potentiometer1);
-
-  delay(1);  // delay in between reads for stability
-}
-
-/* void fade (int time_per, int num_steps){
-  int brightness = 0; // This can be 0-255
+void fade (int time_per, int num_steps, int led){
+  int brightness = 255; // This can be 0-255
   int light_interval = num_steps / 255;
   for(int i = 0; i < num_steps; i++){
-    brightness += light_interval;
-    set_brightness(brightness);
+    brightness -= light_interval;
+    analogWrite(led, (brightness*global_brightness) / 100);
     delay(time_per/num_steps);
   }
-} */
+}
+
+void setLedState(bool on, int led){
+  if(on){
+    /* Serial.print("brightness:");
+    Serial.println((global_brightness*255) / 100);
+    Serial.println(global_brightness); */
+
+    analogWrite(led, global_brightness*2);
+  }else{
+    analogWrite(led, 0);
+  }
+  
+}
+
 
 
 void loop() {
+  //A5 = Potentiometer 1
+  //A4 = Potentiometer 2
+  int potentiometer1 = analogRead(A5);
+  int potentiometer2 = analogRead(A4);
+  global_brightness = potentiometer1 / 10;
 
   if (Serial.available() > 0) {
     // if data is available to read
@@ -81,42 +108,56 @@ void loop() {
     }
   }
 
-  if (ledState == ON) {
-    // put your main code here, to run repeatedly:
-    
-    digitalWrite(RED_LED, HIGH); // sets the digital pin 13 on
-    delay(1000);            // waits for a second
-    digitalWrite(RED_LED, LOW);  // sets the digital pin 13 off
-    delay(1000);            // waits for a second 
+
+
+  if (ledState == ON) {    
+    setLedState(true, RED_LED);
+    delay(50);            // waits for a second
+    setLedState(false, RED_LED);
+    delay(50);            // waits for a second 
   }
 
   else if (ledState == OFF) {
-    digitalWrite(RED_LED, LOW); // Turn red off
+    setLedState(false, RED_LED);
   }
 
+/*
+  Blinks @ 4 Hz for 1 sec, Fade for 1 sec.  Last blink fades to off over Fade.
+*/
   else if (ledState == SLEEP) {
+    for(int i = 0; i < 4; i++){
+      setLedState(true, BLUE_LED);
+      delay(125);
+      setLedState(false, BLUE_LED);
+      delay(125);
+    }
+    fade (1000, 255, BLUE_LED);
+    ledState = OFF;
 
-    
-    
   }
   else if (ledState == RUN) {
-    if (greenLEDBrightness > 0) {
-      analogWrite(GREEN_LED, greenLEDBrightness);
-      greenLEDBrightness =  int(greenLEDBrightness - 255 * (1 - exp(-1.0 / TIME_CONSTANT * 30)));
-      delay(30); // Fade with time constant of 6 seconds
-    }
-    else if (greenLEDBrightness <= 0) {
-      digitalWrite(GREEN_LED, HIGH);
-      delay(500);
-      digitalWrite(GREEN_LED, LOW);
-      delay(500);
-      digitalWrite(GREEN_LED, LOW);
-      delay(500);
-      greenLEDBrightness = 255;
-    }
+    //Fade LED for 6s
+    fade (6000, 255, GREEN_LED);
+
+    //Blink twice for duty cycle of 0.5s
+    setLedState(true, GREEN_LED);
+    delay(500);
+    setLedState(false, GREEN_LED);
+    delay(500);
+    setLedState(true, GREEN_LED);
+    delay(500);
+    setLedState(false, GREEN_LED);
+    delay(500);
   }
+
   else if (ledState == DIAGNOSTIC) {
-    
+    int num_problems = 5;
+    for(int i = 0; i < num_problems; i++){
+      setLedState(true, RED_LED);
+      delay(100);
+      setLedState(true, RED_LED);
+      delay(100);
+    }
   }
   
 }
