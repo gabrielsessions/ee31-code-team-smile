176a1ab0752e762d374637c271823f711184f336
Gabriel Sessions <gsess7732@gmail.com>
2024-02-08 12:50:37 -0500

    Phase 02A updated code with serial control

--- phase2a/motor_control/motor_control.ino
+++ phase2a/motor_control/motor_control.ino
@@ -1,15 +1,41 @@
+#define PORT1 3
+#define PORT2 5
+
 void setup() {
   // motor 0 and 1
-  pinMode(3, OUTPUT);
-  pinMode(5, OUTPUT);
+  pinMode(PORT1, OUTPUT);
+  pinMode(PORT2, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Hello, World!");
+  digitalWrite(PORT2, LOW);
+  analogWrite(PORT1, 100);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(3, HIGH);
-  digitalWrite(5, LOW);
-  delay(1000);
-  digitalWrite(5, HIGH);
-  digitalWrite(3, LOW);
-  delay(1000);
+
+  if (Serial.available() > 0) {
+    // if data is available to read
+    String inputWord = Serial.readStringUntil('\n');
+    Serial.print("You entered: ");
+    Serial.println(inputWord);
+    int newSpeed = inputWord.toInt();
+    moveMotor1(newSpeed);
+  }
+  
+  delay(1);
 }
+
+
+void moveMotor1(int speed) {
+  Serial.println(speed);
+  if (speed > 0) {
+      digitalWrite(PORT2, LOW);
+      analogWrite(PORT1, speed);
+      return;
+  }
+  digitalWrite(PORT1, LOW);
+  analogWrite(PORT2, -speed);
+}
+
