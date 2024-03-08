434749cb3de1130aed3661443d23660dcdb0a344
Gabriel Sessions <gsessi01@eecs.tufts.edu>
2024-03-08 19:30:58 +0000

    Add new file

---
+++ phase3a_part4.ino
@@ -0,0 +1,20 @@
+const int analogPin = A5; // Analog input pin
+const float R1 = 12000.0; // 12kΩ resistor
+const float R2 = 10000.0; // 10kΩ resistor
+const float Vref = 4.8; // Reference voltage (5V for most Arduinos)
+const float ADCmax = 1023.0; // Max ADC value for a 10-bit ADC
+
+void setup() {
+  pinMode(analogPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(analogPin);
+  float voltage = sensorValue * (Vref / ADCmax) * ((R1 + R2) / R2);
+  
+  Serial.print("Battery Voltage: ");
+  Serial.println(voltage);
+  
+  delay(1000); // Delay a second between measurements
+}
